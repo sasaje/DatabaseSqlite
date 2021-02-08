5bd4e8420b34765d8c542a5a708927fa9029e123
sarasandager <sara590x@edu.easj.dk>
2021-02-08 09:36:37 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/*
+ *
+ * Developed by Sara Sandager (sara590x@edu.easj.dk)
+ * Licensed under the MIT License
+ * 07/02/2021
+ *
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Test databaser!");
+    }
+}
